6b9130ec05dbc50c49ea944363763f76fe64905c
machinespray <machinespray1@gmail.com>
2019-01-26 12:57:13 -0500

    Added experimental character select menu!
    (I may have broken the skin select though)

--- Assembly-CSharp.LadybugTestMod.mm/Base.cs
+++ Assembly-CSharp.LadybugTestMod.mm/Base.cs
@@ -13,6 +13,10 @@ namespace Assembly_CSharp
 {
     public static class Utils
     {
+        public static string[] getCharacters()
+        {
+            return new string[] { "SaffronDemo", "RevaDemo", "IreliaCampaign" };
+        }
         /* Combines together a list of paths. */
         public static string CombinePaths(params string[] paths)
         {
@@ -57,7 +61,7 @@ namespace Assembly_CSharp
 
         }
 
-        public static SpriteAnimationClip LoadNewSpriteAnimationClip(string[] FilePaths, float PixelsPerUnit=100.0f, float KeyFrameLength=0.02f)
+        public static SpriteAnimationClip LoadNewSpriteAnimationClip(string[] FilePaths, float PixelsPerUnit = 100.0f, float KeyFrameLength = 0.02f)
         {
             Debug.Log("Hi, welcome to LoadNewSpriteAnimationClip!");
             List<Sprite> sprites = new List<Sprite>();
@@ -234,7 +238,7 @@ namespace Assembly_CSharp
         {
             Debug.Log(String.Format("Getting DataFile {0}", dataFile));
 
-            string targetPath = Path.Combine(Utils.modPath, dataFile);      
+            string targetPath = Path.Combine(Utils.modPath, dataFile);
             /* if it doesn't exist yet, write out the original into persistentDataPath */
             if (!File.Exists(targetPath))
             {
@@ -246,4 +250,76 @@ namespace Assembly_CSharp
         }
     }
 
+    //machinespray messing around with a character selector
+    [MonoModPatch("global::OptionCtrl")]
+    public class patch_RevaDisplay : OptionCtrl
+    {
+        //Store selection information through reloads
+        private static int option = 0;
+        public static string heroID = "SaffronDemo";
+        public static string heroAnim = "Saffron";
+        public static string heroName = "Saffron";
+        [MonoModPublic]
+        private ItemManager itemMan;
+        [MonoModPublic]
+        private BC ctrl;
+
+        [MonoModReplace]
+        public void ClickEnableReva()
+        {
+            option++;
+            if (option > Utils.getCharacters().Length - 1)
+            {
+                option = 0;
+            }
+            this.UpdateRevaDisplay();
+        }
+        [MonoModReplace]
+        private void UpdateOutfitDisplay()
+        {
+        }
+
+        [MonoModReplace]
+        public void UpdateRevaDisplay()
+        {
+            //NPE Prevention
+            bool flag = ctrl.runCtrl.spCtrl == null;
+            revaDisplay.updateMode = AnimatorUpdateMode.UnscaledTime;
+            if (flag)
+            {
+                revaText.text = heroName;
+                revaDisplay.runtimeAnimatorController = this.itemMan.GetAnim(heroAnim);
+                return;
+            }
+            BeingObject hero = ctrl.runCtrl.spCtrl.heroDictionary[Utils.getCharacters()[option]];
+            revaDisplay.runtimeAnimatorController = this.itemMan.GetAnim(hero.animName);
+            revaText.text = hero.nameString;
+            heroID = hero.beingID;
+            heroAnim = hero.animName;
+            heroName = hero.nameString;
+        }
+    }
+    [MonoModPatch("global::SpawnCtrl")]
+    public class patch_SpawnCtrl : SpawnCtrl
+    {
+        public void CreatePlayers()
+        {
+            if (S.I.AUTOMATION)
+            {
+                Player component = this.PlaceBeing(patch_RevaDisplay.heroID, TI.tileGrid[this.playerSpawnPos[0].x, this.playerSpawnPos[0].y], 0, false).GetComponent<Player>();
+                component.gameObject.GetComponent<Animator>().runtimeAnimatorController = this.GetAnim(patch_RevaDisplay.heroAnim);
+                component.StartCoroutine(component.TestLoop());
+            }
+            else
+            {
+                Being hero = PlaceBeing(patch_RevaDisplay.heroID, TI.tileGrid[this.playerSpawnPos[0].x, this.playerSpawnPos[0].y], 0, false);
+                hero.gameObject.GetComponent<Animator>().runtimeAnimatorController = this.GetAnim(patch_RevaDisplay.heroAnim);
+            }
+            if (S.I.FAIRY)
+            {
+                this.CreateFairy();
+            }
+        }
+
+    }
 }
